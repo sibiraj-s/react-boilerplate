589269fdc972d2684319fd3e81eb909638143664
sibiraj-s <dev.sibiraj@outlook.com>
2020-07-05 17:07:25 +0530

    chore(packages): update dependencies

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,12 +1,7 @@
-const glob = require('glob');
-
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
-const Paths = require('./utils/paths');
 
 module.exports = {
   mode: 'production',
@@ -19,10 +14,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'styles/[name]-[contenthash].css',
       chunkFilename: 'styles/[name]-[contenthash].css'
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync(`${Paths.srcDir}/**/*`, { nodir: true }),
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     }),
     new webpack.HashedModuleIdsPlugin()
   ],
