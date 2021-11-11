f53aadfda8140a5bd44f7cc190ded0c4cc699a75
sibiraj-s <dev.sibiraj@outlook.com>
2021-11-12 00:45:48 +0530

    chore: prefer node protocol

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const path = require('path');
-
+const path = require('node:path');
 const { ProgressPlugin } = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
