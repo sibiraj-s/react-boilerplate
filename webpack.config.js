2ff5d699aa9ec641544591dfac0c2d0d0311b0b2
sibiraj-s <dev.sibiraj@outlook.com>
2021-11-12 00:29:50 +0530

    build: simplify webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 
-const pkgJson = require('./package.json');
 const Paths = require('./webpack/utils/paths');
-const { WEBPACK_MODE, isProduction } = require('./webpack/utils/env');
 const devConfig = require('./webpack/dev.config');
 const prodConfig = require('./webpack/prod.config');
+
+const isDevEnv = process.env.NODE_ENV === 'development';
 
 const baseConfig = {
   context: Paths.rootDir,
@@ -22,7 +22,7 @@ const baseConfig = {
   entry: path.resolve(Paths.srcDir, 'index.js'),
   output: {
     path: Paths.outDir,
-    publicPath: '/',
+    publicPath: 'auto',
     clean: true,
   },
   module: {
@@ -35,7 +35,7 @@ const baseConfig = {
       {
         test: /\.s?css$/,
         use: [
-          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          isDevEnv ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           'sass-loader',
@@ -45,7 +45,7 @@ const baseConfig = {
         test: /\.svg$/i,
         type: 'asset/resource',
         generator: {
-          filename: isProduction ? 'images/[name]-[contenthash][ext][query]' : '[name][ext]',
+          filename: isDevEnv ? '[name][ext]' : 'images/[name]-[contenthash][ext][query]',
         },
       },
     ],
@@ -73,9 +73,4 @@ const baseConfig = {
   },
 };
 
-// changes required to deploy in Github Pages
-if (process.env.NODE_ENV === 'gh-pages') {
-  baseConfig.output.publicPath = `/${pkgJson.name}/`;
-}
-
-module.exports = merge(baseConfig, WEBPACK_MODE === 'development' ? devConfig : prodConfig);
+module.exports = merge(baseConfig, isDevEnv ? devConfig : prodConfig);
