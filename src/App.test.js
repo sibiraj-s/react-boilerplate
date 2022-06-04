105b3e0395a95416b612a507174922954052f954
Sibiraj <dev.sibiraj@outlook.com>
2022-06-04 20:16:25 +0530

    test: prefer react 18's createRoot api

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
 import { describe, it, expect } from '@jest/globals';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     expect(App).toBeInstanceOf(Function);
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const root = ReactDOM.createRoot(div);
+    root.render(<App />);
+    root.unmount();
   });
 });
